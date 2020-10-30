8c70326ec40f3f47a7a6cc8de4035b0d263150b8
xdebbie <deh@live.in>
2020-10-30 16:01:05 +0100

    Auth now working for logins

--- app/config.js
+++ app/config.js
@@ -1,14 +1,25 @@
 // --------------------------------------------------------------------------------------------------------------------
 // CONFIGURATION DE L'APPLICATION
 // --------------------------------------------------------------------------------------------------------------------
+// let provider = new firebase.auth.GithubAuthProvider();
 
 const config = {
-    // Configuration de la plateforme Firebase
-    firebase: {
-        // ...
-    },
+	// Configuration de la plateforme Firebase
+	firebase: {
+		apiKey: 'AIzaSyC324Xiij30qhCO1tV8_z6aTKMzox0nox0',
+		authDomain: 'vanilla-router.firebaseapp.com',
+		databaseURL: 'https://vanilla-router.firebaseio.com',
+		projectId: 'vanilla-router',
+		storageBucket: 'vanilla-router.appspot.com',
+		messagingSenderId: '1063902923009',
+		appId: '1:1063902923009:web:a0923aa2c78b766d20b300',
+		measurementId: 'G-S53CR2XC2Z',
+	},
 };
 
+if (!firebase.apps.length) {
+	firebase.initializeApp(config.firebase);
+}
 
 // La configuration est exportée afin d'être accessible par d'autres modules.
 export default config;
--- controllers/Login.js
+++ controllers/Login.js
@@ -2,4 +2,54 @@ export default class Login {
 	constructor() {
 		this.view = '../views/login.html';
 	}
+
+	executeHttpRequest() {
+		// Login APIs
+		// Google
+		document.querySelector('#googleLogin').addEventListener('click', () => {
+			let provider = new firebase.auth.GoogleAuthProvider();
+			firebase
+				.auth()
+				.signInWithPopup(provider)
+				.then((user) => {
+					// vous pouvez récupérer le nom comme ceci :
+					alert(user.additionalUserInfo.profile.name);
+				})
+				.catch(function (error) {
+					console.log(error);
+				});
+		});
+
+		// Facebook
+		document
+			.querySelector('#facebookLogin')
+			.addEventListener('click', () => {
+				let provider = new firebase.auth.FacebookAuthProvider();
+				firebase
+					.auth()
+					.signInWithPopup(provider)
+					.then((user) => {
+						// vous pouvez récupérer le nom comme ceci :
+						alert(user.additionalUserInfo.profile.name);
+					})
+					.catch(function (error) {
+						console.log(error);
+					});
+			});
+
+		// Github
+		document.querySelector('#githubLogin').addEventListener('click', () => {
+			let provider = new firebase.auth.GithubAuthProvider();
+			firebase
+				.auth()
+				.signInWithPopup(provider)
+				.then((user) => {
+					// vous pouvez récupérer le nom comme ceci :
+					alert(user.additionalUserInfo.profile.name);
+				})
+				.catch(function (error) {
+					console.log(error);
+				});
+		});
+	}
 }
